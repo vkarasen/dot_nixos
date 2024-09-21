44e49039f5e9d4535f2887091745212638b2bd29
Vitali Karasenko <vkarasen@gmail.com>
2024-09-21 13:07:21 +0100

    initial push

---
+++ README.md
@@ -0,0 +1,6 @@
+btw I use Nix
+
+# Bootstrapping
+Install Nix: https://nixos.org/download/
+
+`nix run home-manager -- switch --flake github:vkarasen/dot_nixos#vkarasen`
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+	description = "I am a very special flake";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+		home-manager = {
+			url = "github:nix-community/home-manager/master";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		nix-index-database = {
+			url = "github:nix-community/nix-index-database";
+    		inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = inputs@{
+		nixpkgs,
+		home-manager,
+		nix-index-database,
+		...
+	}: let
+
+		system = "x86_64-linux";
+
+		pkgs = nixpkgs.legacyPackages.${system};
+
+	in {
+
+		homeConfigurations.vkarasen = home-manager.lib.homeManagerConfiguration {
+
+			inherit pkgs;
+
+			modules = [
+			  nix-index-database.hmModules.nix-index
+			  { programs.nix-index-database.comma.enable = true; }
+			];
+		};
+	};
+}
