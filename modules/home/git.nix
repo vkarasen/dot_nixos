6cf6be22d9896a1bb565262dc2b205ca3e16967f
Vitali Karasenko <vkarasen@gmail.com>
2024-09-22 21:21:55 +0100

    more git config stuff + shelltools

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -14,11 +14,30 @@
 			userName = "Vitali Karasenko";
 
 			extraConfig = {
+				core = {
+					pager = "delta";
+				};
+				interactive = {
+					diffFilter = "delta --color-only";
+				};
+				delta = {
+					navigate = true;
+					features = "side-by-side line-numbers decorations";
+				};
+				merge = {
+					conflictstyle = "diff3";
+				};
+				diff = {
+					colorMoved = "default";
+				};
 				pull = {
 					rebase = true;
 				};
 				init = {
 					defaultBranch = "main";
+				};
+				pager = {
+					difftool = "true";
 				};
 			};
 
--- modules/home/shellPackages.nix
+++ modules/home/shellPackages.nix
@@ -1,7 +1,17 @@
-{pkgs, ...}: {
+{config, lib, pkgs, ...}: {
 	home.packages = with pkgs; [
 		nh
 		nix-output-monitor
 		nvd
+		ripgrep
+		fzf
+		curl
+		wget
+		bat
+		delta
+		gnused
+		tig
 	];
+
+
 }
