3c5ab23de16939b599f561d8157c5c846bb3befb
Vitali Karasenko <vkarasen@gmail.com>
2024-09-26 00:43:47 +0100

    expanded on prelude, now mostly migrated

--- modules/home/neovim/prelude.nix
+++ modules/home/neovim/prelude.nix
@@ -1,10 +1,47 @@
 # Put lua config here that should be loaded first
-{...}: {
-	programs.neovim.extraLuaConfig = /* lua */  ''
+{ ... }:
+{
+  programs.neovim.extraLuaConfig = # lua
+    ''
 
-		vim.g.mapleader = ";"
-		vim.g.maplocalleader = " "
-		vim.o.number = true
-		vim.o.relativenumber = true
-	'';
+      	vim.g.mapleader = ';'
+      	vim.g.maplocalleader = ' '
+      	vim.o.number = true
+      	vim.o.relativenumber = true
+      	vim.o.cursorline = true
+      	vim.o.list = true
+      	vim.opt.listchars = {
+      	    trail = '␣',
+      	    extends = '⇉',
+      	    precedes = '⇇',
+      	    nbsp = '·',
+      	    tab = '▏┈',
+      	    leadmultispace = '▏┈',
+      	}
+      	vim.o.sts = 4
+      	vim.o.sw = 4
+
+      	vim.o.infercase = true
+      	vim.o.ignorecase = true
+      	vim.o.smartcase = true
+      	vim.o.gdefault = true
+      	vim.o.linebreak = true
+
+      	vim.keymap.set('n', 'j', '20j')
+      	vim.keymap.set('n', 'j', 'gj')
+      	vim.keymap.set('n', 'k', '20k')
+      	vim.keymap.set('n', 'k', 'gk')
+      	vim.keymap.set('n', 'H', '<c-w>h')
+      	vim.keymap.set('n', 'L', '<c-w>l')
+      	vim.keymap.set('n', '<c-i>', '<c-]>')
+      	vim.keymap.set('n', '<c-h>', 'gT')
+      	vim.keymap.set('n', '<c-l>', 'gt')
+
+      	vim.keymap.set('n', '<c-u>', '<cmd>nohls<cr>', { silent = true})
+      	vim.keymap.set('n', 'U', '<c-r>')
+
+      	vim.keymap.set('n', 'Q', 'gqap')
+      	vim.keymap.set('v', 'Q', 'gq')
+
+    '';
 }
