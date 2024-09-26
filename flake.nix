37ce87d14787be57a21a906e5388cc1f6db6b3a1
Vitali Karasenko <vkarasen@gmail.com>
2024-09-26 20:46:57 +0100

    formatting, added nix system formatter

--- flake.nix
+++ flake.nix
@@ -1,47 +1,44 @@
 {
-	description = "I am a very special flake";
+  description = "I am a very special flake";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-		catppuccin.url = "github:catppuccin/nix";
+    catppuccin.url = "github:catppuccin/nix";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/master";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-		nix-index-database = {
-			url = "github:nix-community/nix-index-database";
-    		inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = inputs@{
-		nixpkgs,
-		home-manager,
-		nix-index-database,
-		catppuccin,
-		...
-	}: let
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    nix-index-database,
+    catppuccin,
+    ...
+  }: let
+    system = "x86_64-linux";
 
-		system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations.vkarasen = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
 
-		pkgs = nixpkgs.legacyPackages.${system};
+      modules = [
+        ./home-manager
+        nix-index-database.hmModules.nix-index
+        {programs.nix-index-database.comma.enable = true;}
+        catppuccin.homeManagerModules.catppuccin
+      ];
+    };
 
-	in {
-
-		homeConfigurations.vkarasen = home-manager.lib.homeManagerConfiguration {
-
-			inherit pkgs;
-
-			modules = [
-			  ./home-manager
-			  nix-index-database.hmModules.nix-index
-			  { programs.nix-index-database.comma.enable = true; }
-			  catppuccin.homeManagerModules.catppuccin
-			];
-
-		};
-	};
+    formatter.${system} = pkgs.alejandra;
+  };
 }
