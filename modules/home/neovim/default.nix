8ce2bf7143cc486a5aaa3d1de71af55db77beddd
Vitali Karasenko <vkarasen@gmail.com>
2024-10-29 00:45:06 +0000

    removed some obsolete keymaps
    you're not a child anymore, learn proper vim movements

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -48,24 +48,13 @@
         smartcase = true;
         gdefault = true;
         linebreak = true;
-		acd = true;
       };
 
       keymaps = [
         {
           mode = "n";
-          key = "J";
-          action = "20j";
-        }
-        {
-          mode = "n";
           key = "j";
           action = "gj";
-        }
-        {
-          mode = "n";
-          key = "K";
-          action = "20k";
         }
         {
           mode = "n";
@@ -94,24 +83,8 @@
         }
         {
           mode = "n";
-          key = "<c-u>";
-          action = "<cmd>nohls<cr>";
-          options.silent = true;
-        }
-        {
-          mode = "n";
           key = "U";
           action = "<c-r>";
-        }
-        {
-          mode = "n";
-          key = "<c-h>";
-          action = "gT";
-        }
-        {
-          mode = "n";
-          key = "<c-l>";
-          action = "gt";
         }
         {
           mode = "n";
