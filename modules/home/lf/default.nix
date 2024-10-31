af5af5320b5c366140cc40eb8187abf72aac7e55
Vitali Karasenko <vkarasen@gmail.com>
2024-10-31 01:33:56 +0000

    lf: add bash integration
    * uses this https://github.com/gokcehan/lf/blob/master/etc/lfcd.sh

--- modules/home/lf/default.nix
+++ modules/home/lf/default.nix
@@ -18,95 +18,110 @@
 
     xdg.configFile."lf/icons".source = ./icons;
 
-    programs.lf = {
-      enable = true;
-      settings = {
-        preview = true;
-        hidden = false;
-        drawbox = true;
-        icons = true;
-        ignorecase = true;
+    programs = {
+      bash = {
+        initExtra =
+          #bash
+          ''
+            lfcd () {
+                # `command` is needed in case `lfcd` is aliased to `lf`
+                cd "$(command lf -print-last-dir "$@")"
+            }
+          '';
+        shellAliases = {
+          lf = "lfcd";
+        };
       };
+      lf = {
+        enable = true;
+        settings = {
+          preview = true;
+          hidden = false;
+          drawbox = true;
+          icons = true;
+          ignorecase = true;
+        };
 
-      # taken from https://github.com/gokcehan/lf/wiki/Integrations
-      commands = {
-        z = ''
-          %{{
-              result="$(zoxide query --exclude "$PWD" "$@" | sed 's/\\/\\\\/g;s/"/\\"/g')"
+        # taken from https://github.com/gokcehan/lf/wiki/Integrations
+        commands = {
+          z = ''
+            %{{
+                result="$(zoxide query --exclude "$PWD" "$@" | sed 's/\\/\\\\/g;s/"/\\"/g')"
+                lf -remote "send $id cd \"$result\""
+            }}
+          '';
+          zi = ''
+            ''${{
+              result="$(zoxide query -i | sed 's/\\/\\\\/g;s/"/\\"/g')"
               lf -remote "send $id cd \"$result\""
-          }}
-        '';
-        zi = ''
-          ''${{
-            result="$(zoxide query -i | sed 's/\\/\\\\/g;s/"/\\"/g')"
-            lf -remote "send $id cd \"$result\""
-          }}
-        '';
-        fzf_jump = ''
-          ''${{
-                res="$(find . -maxdepth 1 | fzf --reverse --header="Jump to location")"
-                if [ -n "$res" ]; then
-                  if [ -d "$res" ]; then
-                    cmd="cd"
-                  else
-                    d="select"
-                  fi
-                  res="$(printf '%s' "$res" | sed 's/\\/\\\\/g;s/"/\\"/g')"
-                  lf -remote "send $id $cmd \"$res\""
-              fi
-          }}
-        '';
-        fzf_search = ''
-          ''${{
-                  cmd="rg --column --line-number --no-heading --color=always --smart-case"
-                  fzf --ansi --disabled --layout=reverse --header="Search in files" --delimiter=: \
-                          --bind="start:reload([ -n {q} ] && $cmd -- {q} || true)" \
-                          --bind="change:reload([ -n {q} ] && $cmd -- {q} || true)" \
-                          --bind='enter:become(lf -remote "send $id select \"''$(printf "%s" {1} | sed '\'''s/\\/\\\\/g;s/"/\\"/g'\''')\"")' \
-                          --preview='bat --color=always --highlight-line={2} -- {1}'
-          }}
-        '';
-        mkdir = ''
-          ''${{
-                printf "File: "
-                read DIR
-                mkdir $DIR
-          	}}
-        '';
-		mkfile = ''
-          ''${{
-                printf "File: "
-                read FILE
-				touch "$FILE"
-          	}}
-				'';
+            }}
+          '';
+          fzf_jump = ''
+            ''${{
+                  res="$(find . -maxdepth 1 | fzf --reverse --header="Jump to location")"
+                  if [ -n "$res" ]; then
+                    if [ -d "$res" ]; then
+                      cmd="cd"
+                    else
+                      d="select"
+                    fi
+                    res="$(printf '%s' "$res" | sed 's/\\/\\\\/g;s/"/\\"/g')"
+                    lf -remote "send $id $cmd \"$res\""
+                fi
+            }}
+          '';
+          fzf_search = ''
+            ''${{
+                    cmd="rg --column --line-number --no-heading --color=always --smart-case"
+                    fzf --ansi --disabled --layout=reverse --header="Search in files" --delimiter=: \
+                            --bind="start:reload([ -n {q} ] && $cmd -- {q} || true)" \
+                            --bind="change:reload([ -n {q} ] && $cmd -- {q} || true)" \
+                            --bind='enter:become(lf -remote "send $id select \"''$(printf "%s" {1} | sed '\'''s/\\/\\\\/g;s/"/\\"/g'\''')\"")' \
+                            --preview='bat --color=always --highlight-line={2} -- {1}'
+            }}
+          '';
+          mkdir = ''
+            ''${{
+                  printf "File: "
+                  read DIR
+                  mkdir $DIR
+            	}}
+          '';
+          mkfile = ''
+                  ''${{
+                        printf "File: "
+                        read FILE
+            touch "$FILE"
+                  	}}
+          '';
+        };
+
+        previewer.source = config.xdg.dataFile."lf/pv.sh".source;
+
+        keybindings = {
+          "gff" = ":fzf_jump";
+          "gfg" = ":fzf_search";
+          "A" = "mkdir";
+          "a" = "mkfile";
+          "." = "set hidden!";
+          "D" = "delete";
+        };
+
+        extraConfig =
+          # put on-cd/on-select cmd extensions here
+          ''
+            cmd on-cd &{{
+                zoxide add "$PWD"
+            }}
+            cmd on-select &{{
+                lf -remote "send $id set statfmt \"$(eza -ld --color=always "$f" | sed 's/\\/\\\\/g;s/"/\\"/g')\""
+            }}
+            cmd on-cd &{{
+                fmt="$(STARSHIP_SHELL= starship prompt | sed 's/\\/\\\\/g;s/"/\\"/g')"
+                lf -remote "send $id set promptfmt \"$fmt\""
+            }}
+          '';
       };
-
-      previewer.source = config.xdg.dataFile."lf/pv.sh".source;
-
-      keybindings = {
-        "gff" = ":fzf_jump";
-        "gfg" = ":fzf_search";
-        "A" = "mkdir";
-        "a" = "mkfile";
-        "." = "set hidden!";
-        "D" = "delete";
-      };
-
-      extraConfig =
-        # put on-cd/on-select cmd extensions here
-        ''
-          cmd on-cd &{{
-              zoxide add "$PWD"
-          }}
-          cmd on-select &{{
-              lf -remote "send $id set statfmt \"$(eza -ld --color=always "$f" | sed 's/\\/\\\\/g;s/"/\\"/g')\""
-          }}
-          cmd on-cd &{{
-              fmt="$(STARSHIP_SHELL= starship prompt | sed 's/\\/\\\\/g;s/"/\\"/g')"
-              lf -remote "send $id set promptfmt \"$fmt\""
-          }}
-        '';
     };
   };
 }
