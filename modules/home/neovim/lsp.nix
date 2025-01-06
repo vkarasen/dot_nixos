e153acc89b3ac8a5ad0e7f3bc6aea4096f5cf779
Vitali Karasenko <vkarasen@gmail.com>
2025-01-06 01:40:39 +0000

    neovim: improved lsp behavior

--- modules/home/neovim/lsp.nix
+++ modules/home/neovim/lsp.nix
@@ -44,13 +44,18 @@ in {
           enable = true;
           autoEnableSources = true;
 
-          settings.sources = [
-            {name = "nvim_lsp";}
-            {name = "luasnip";}
-            {name = "path";}
-            {name = "buffer";}
-          ];
           settings = {
+            sources = [
+              {name = "nvim_lsp";}
+              {name = "luasnip";}
+              {name = "path";}
+              {name = "buffer";}
+              {
+                name = "buffer";
+                # Words from other open buffers can also be suggested.
+                option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+              }
+            ];
             snippet.expand =
               #lua
               ''
@@ -59,6 +64,15 @@ in {
                 end
 
               '';
+            mapping = {
+              "<C-u>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-d>" = "cmp.mapping.scroll_docs(4)";
+              "<C-Space>" = "cmp.mapping.complete()";
+              "<C-e>" = "cmp.mapping.close()";
+              "<C-n>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+              "<C-p>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping.confirm({ select = true })";
+            };
           };
         };
         luasnip.enable = true;
