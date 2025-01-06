9ae7fb9cbc3b07bd5a81d3ff7766416d8f9c062a
Vitali Karasenko <vkarasen@gmail.com>
2025-01-06 01:27:48 +0000

    neovim: add harpoon

--- modules/home/neovim/telescope.nix
+++ modules/home/neovim/telescope.nix
@@ -1,14 +1,27 @@
 {...}: {
   programs = {
     nixvim = {
-      plugins.telescope = {
-        enable = true;
-        keymaps = {
-          "<leader>ff" = "find_files";
-          "<leader>fg" = "live_grep";
-          "<leader>fb" = "buffers";
-          "<leader>fh" = "help_tags";
-          "<leader>fo" = "oldfiles";
+      plugins = {
+        telescope = {
+          enable = true;
+          keymaps = {
+            "<leader>ff" = "find_files";
+            "<leader>fg" = "live_grep";
+            "<leader>fb" = "buffers";
+            "<leader>fh" = "help_tags";
+            "<leader>fo" = "oldfiles";
+          };
+        };
+        harpoon = {
+          enable = true;
+          enableTelescope = true;
+          keymaps = {
+            addFile = "<leader>ha";
+            navNext = "<leader>hn";
+            navPrev = "<leader>hp";
+            toggleQuickMenu = "<leader>fp";
+            cmdToggleQuickMenu = "<leader>fP";
+          };
         };
       };
     };
