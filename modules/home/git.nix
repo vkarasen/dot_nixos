73bd6ab258c1430427d43a28f9f415658655339a
Vitali Karasenko <vkarasen@gmail.com>
2025-02-25 15:08:10 +0000

    git: more config stuff
    adopted from https://blog.gitbutler.com/how-git-core-devs-configure-git/

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -17,6 +17,8 @@
       extraConfig = {
         core = {
           pager = "delta";
+          fsmonitor = true;
+          untrackedCache = true;
         };
         interactive = {
           diffFilter = "delta --color-only";
@@ -31,15 +33,48 @@
         };
         diff = {
           colorMoved = "default";
+          algorithm = "histogram";
+          mnemonicPrefix = true;
+          renames = true;
         };
         pull = {
           rebase = true;
         };
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+          updateRefs = true;
+        };
+        push = {
+          autoSetupRemote = true;
+          followTags = true;
+        };
+        fetch = {
+          prune = true;
+          pruneTags = true;
+          all = true;
+        };
         init = {
           defaultBranch = "main";
         };
+        help = {
+          autocorrect = "prompt";
+        };
+        rerere = {
+          enabled = true;
+          autoupdate = true;
+        };
         pager = {
           difftool = "true";
+        };
+        column = {
+          ui = "auto";
+        };
+        branch = {
+          sort = "-committerdate";
+        };
+        tag = {
+          sort = "version:refname";
         };
       };
     };
