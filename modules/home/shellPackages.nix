5f18f20b32ef08aa6f60ac4ed4539d1b7c871e49
Vitali Karasenko <vkarasen@gmail.com>
2025-03-28 09:18:54 +0000

    bat: don't need to explicitly install catppuccin theme anymore

--- modules/home/shellPackages.nix
+++ modules/home/shellPackages.nix
@@ -40,17 +40,6 @@
       zoxide.enable = true;
       bat = {
         enable = true;
-        themes = {
-          catppuccin-mocha = {
-            src = pkgs.fetchFromGitHub {
-              owner = "catppuccin";
-              repo = "bat";
-              rev = "d3feec47b16a8e99eabb34cdfbaa115541d374fc";
-              sha256 = "1g73x0p8pbzb8d1g1x1fwhwf05sj3nzhbhb65811752p5178fh5k";
-            };
-            file = "themes/Catppuccin Mocha.tmTheme";
-          };
-        };
         config = {
           theme = "Catppuccin Mocha";
         };
