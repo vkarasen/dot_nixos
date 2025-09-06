2c442f041c9784a2975304b0fdc613ad149ee012
Vitali Karasenko <vkarasen@gmail.com>
2025-09-06 11:33:06 +0100

    added gh

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -9,6 +9,12 @@
     ];
 
     programs = {
+      gh = {
+        enable = true;
+        gitCredentialHelper = {
+          enable = true;
+        };
+      };
       git = {
         enable = true;
 
