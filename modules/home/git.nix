7716903d92adee853d78ab5abdb8fb18dd972d67
Vitali Karasenko <vkarasen@gmail.com>
2025-09-06 11:16:45 +0100

    changed format of modules/home/git.nix
    * no functional changes

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   pkgs,
   config,
   ...
@@ -9,77 +8,79 @@
       delta
     ];
 
-    programs.git = {
-      enable = true;
+    programs = {
+      git = {
+        enable = true;
 
-      userEmail = config.my.git.email;
-      userName = "Vitali Karasenko";
+        userEmail = config.my.git.email;
+        userName = "Vitali Karasenko";
 
-      extraConfig = {
-        core = {
-          pager = "delta";
-          fsmonitor = true;
-          untrackedCache = true;
-          compression = 9;
-          whitespace = "error";
-          preloadindex = true;
-        };
-        interactive = {
-          diffFilter = "delta --color-only";
-        };
-        delta = {
-          navigate = true;
-          features = "side-by-side line-numbers decorations";
-          syntax-theme = "Catppuccin Mocha";
-        };
-        merge = {
-          conflictstyle = "zdiff3";
-        };
-        diff = {
-          colorMoved = "default";
-          algorithm = "histogram";
-          mnemonicPrefix = true;
-          renames = true;
-        };
-        pull = {
-          rebase = true;
-        };
-        rebase = {
-          autoSquash = true;
-          autoStash = true;
-          updateRefs = true;
-          missingCommitsCheck = "warn";
-        };
-        push = {
-          autoSetupRemote = true;
-          followTags = true;
-        };
-        fetch = {
-          prune = true;
-          pruneTags = true;
-          all = true;
-        };
-        init = {
-          defaultBranch = "main";
-        };
-        help = {
-          autocorrect = "prompt";
-        };
-        rerere = {
-          enabled = true;
-          autoupdate = true;
-        };
-        pager = {
-          difftool = "true";
-        };
-        column = {
-          ui = "auto";
-        };
-        branch = {
-          sort = "-committerdate";
-        };
-        tag = {
-          sort = "version:refname";
+        extraConfig = {
+          core = {
+            pager = "delta";
+            fsmonitor = true;
+            untrackedCache = true;
+            compression = 9;
+            whitespace = "error";
+            preloadindex = true;
+          };
+          interactive = {
+            diffFilter = "delta --color-only";
+          };
+          delta = {
+            navigate = true;
+            features = "side-by-side line-numbers decorations";
+            syntax-theme = "Catppuccin Mocha";
+          };
+          merge = {
+            conflictstyle = "zdiff3";
+          };
+          diff = {
+            colorMoved = "default";
+            algorithm = "histogram";
+            mnemonicPrefix = true;
+            renames = true;
+          };
+          pull = {
+            rebase = true;
+          };
+          rebase = {
+            autoSquash = true;
+            autoStash = true;
+            updateRefs = true;
+            missingCommitsCheck = "warn";
+          };
+          push = {
+            autoSetupRemote = true;
+            followTags = true;
+          };
+          fetch = {
+            prune = true;
+            pruneTags = true;
+            all = true;
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          help = {
+            autocorrect = "prompt";
+          };
+          rerere = {
+            enabled = true;
+            autoupdate = true;
+          };
+          pager = {
+            difftool = "true";
+          };
+          column = {
+            ui = "auto";
+          };
+          branch = {
+            sort = "-committerdate";
+          };
+          tag = {
+            sort = "version:refname";
+          };
         };
       };
     };
