60706de6361856ec6ecdf14f4fda912ad9896c42
Vitali Karasenko <vkarasen@gmail.com>
2026-05-03 10:54:54 +0100

    Revert "tmux: add optnix as a popup"
    This reverts commit 8683e65df04ca4380b7368968de79165e09cc214.

--- modules/home/tmux/default.nix
+++ modules/home/tmux/default.nix
@@ -41,8 +41,7 @@
             set -g renumber-windows on
             bind f run -b "${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf/scripts/window.sh switch"
             bind M-c attach-session -c "#{pane_current_path}"
-            bind M-l display-popup -d "#{pane_current_path}" -w 90% -h 90% -E "lfk"
-            bind M-o display-popup -d "#{pane_current_path}" -w 90% -h 90% -E "optnix"
+            bind M-l display-popup -d "#{pane_current_path}" -w 90% -h 90% -E "lf"
           '';
       };
 
